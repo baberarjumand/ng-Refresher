a66c2544120e780ff5a0dc0ed342ac8b2bd860cb
baberarjumand <baber.arjumand@gmail.com>
2020-02-11 14:39:12 +0300

    completed exercise 2.14, local refs

--- src/app/persons/person-input.component.ts
+++ src/app/persons/person-input.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 
 export class PersonInputComponent {
 
-  onCreatePerson() {
-    console.log('Created a person!');
+  onCreatePerson(personName: string) {
+    console.log('Created a person: ' + personName);
   }
 
 }
