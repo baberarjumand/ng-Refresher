7729fb6900c37e18b3d561f9739f604ace5389ee
baberarjumand <baber.arjumand@gmail.com>
2020-02-15 13:48:38 +0300

    completed exercise 2.24, pushing data around

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.css']
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
 
   personList: string[];
   // private personsService: PersonsService;
+  private personsListSubscription: Subscription;
 
   constructor(private personsService: PersonsService) {
     // this.personList = personsService.persons;
@@ -18,6 +20,14 @@ export class PersonsComponent implements OnInit {
 
   ngOnInit(): void {
     this.personList = this.personsService.persons;
+    this.personsListSubscription = this.personsService.personsChanged.subscribe(persons => {
+      this.personList = persons;
+    });
+  }
+
+  ngOnDestroy(): void {
+    // throw new Error('Method not implemented.');
+    this.personsListSubscription.unsubscribe();
   }
 
   onRemovePerson(person: string) {
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,18 +7,21 @@ import { Injectable } from '@angular/core';
 
 export class PersonsService {
 
+  personsChanged = new Subject<string[]>();
   persons: string[] = ['Max', 'Manuel', 'Anna'];
 
   addPerson(name: string) {
     this.persons.push(name);
-    console.log(this.persons);
+    this.personsChanged.next(this.persons);
+    // console.log(this.persons);
   }
 
   removePerson(name: string) {
     this.persons = this.persons.filter(person => {
       return person !== name;
     });
-    console.log(this.persons);
+    this.personsChanged.next(this.persons);
+    // console.log(this.persons);
   }
 
 }
